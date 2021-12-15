4a1c1fadf17525db312530b53a3707aee0943a22
Cédric Gouygou <gouygou.cedric@gmail.com>
2021-12-15 22:39:34 +0100

    maj day15

---
+++ 2021/day15.py
@@ -0,0 +1,71 @@
+import os
+import time
+from dijkstra import *
+os.chdir("/home/cedric/Travail/AlgoInfo/CodesPython/adventofcode/aoc/2021/")
+
+data_str = [list(map(int, list(line))) for line in open("input_day15.txt").read().splitlines()]
+
+
+## Part 1 (heuristique sud + est only)
+
+def lowest_total_risk(tab):
+    height, width = len(tab), len(tab[0])
+
+    T = [width * [0] for _ in range(height)]
+
+    for i in range(height):
+        for j in range(width):
+            if i+j > 0:
+                if i == 0:
+                    T[i][j] = T[i][j-1] + tab[i][j]
+                elif j == 0:
+                    T[i][j] = T[i-1][j] + tab[i][j]
+                else:
+                    T[i][j] = min(T[i][j-1] + tab[i][j], T[i-1][j] + tab[i][j])
+
+    return T[-1][-1]
+
+# print(lowest_total_risk(data_str))
+
+## Part 2
+
+def cycle(value, n):
+    if value + n > 9:
+        return value + n - 9
+    else:
+        return value +n
+
+def shift(tab, i, j):
+    return [[cycle(v, i+j) for v in line] for line in tab]
+
+def concat_tabs(tab, i):
+    tab_line = shift(tab, i, 0)
+    for j in range(1, 5):
+        for k in range(len(tab)):
+            tab_line[k] += shift(tab, i, j)[k]
+    return tab_line
+
+def create_map(tab):
+    full = []
+    for i in range(5):
+        full += concat_tabs(tab, i)
+    return full
+
+def voisins(s):
+    lst = []
+    i, j = s
+    if i > 0:
+        lst.append([full_map[i-1][j], (i-1, j)])
+    if i < len(full_map)-1:
+        lst.append([full_map[i+1][j], (i+1, j)])
+    if j > 0:
+        lst.append([full_map[i][j-1], (i, j-1)])
+    if j < len(full_map[0])-1:
+        lst.append([full_map[i][j+1], (i, j+1)])
+    return lst
+
+full_map = create_map(data_str)
+d, p = dijkstra((0,0), (499, 499), voisins)
+
+print(d)
+
--- 2021/day6.py
+++ 2021/day6.py
@@ -4,9 +4,9 @@ os.chdir("/home/cedric/Travail/AlgoInfo/CodesPython/adventofcode/aoc/2021/")
 data_str = open("input_day6.txt").read().splitlines()
 fishes = list(map(int, data_str[0].split(',')))
 # Example
-fishes = [3, 4, 3, 1, 2]
+# fishes = [3, 4, 3, 1, 2]
 
-nb_days = 18
+nb_days = 256
 
 ## Part 1
 
@@ -23,15 +23,30 @@ nb_days = 18
 
 ## Part 2
 
-def create(timer, days):
-    new = max(0, (days-timer)//7 + 1*((days-timer)%7>0))
-    # print(new)
-    c = new
-    for k in range(new):
-        c += create(8, days-(timer+k*7))
-    return c
+# def create(timer, days):
+#     new = max(0, (days-timer)//7 + 1*((days-timer)%7>0))
+#     # print(new)
+#     c = new
+#     for k in range(new):
+#         c += create(8, days-(timer+k*7))
+#     return c
+#
+# for fish in [3,4,3,1,2]:
+#     print(create(fish, 18))
+#
+# print(sum([create(fish, 256) for fish in [3,4,3,1,2]]))
 
-for fish in [3,4,3,1,2]:
-    print(create(fish, 18))
+timer = {k: 0 for k in range(9)}
 
-print(sum([create(fish, 256) for fish in [3,4,3,1,2]]))
+for f in fishes:
+    timer[f] += 1
+
+for _ in range(nb_days):
+    temp = timer[0]
+    for k in range(8):
+        timer[k] = timer[k+1]
+    timer[6] += temp
+    timer[8] = temp
+    # print(timer)
+
+print(sum(timer.values()))
